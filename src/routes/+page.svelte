17912cc250c93d986e8cea7eb658fa6bdc059af9
Boon <boonsvj2016@gmail.com>
2025-08-28 13:05:40 +0100

    Hopyfully first working prot

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,34 @@
 <script lang="ts">
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcomeFallback from '$lib/images/svelte-welcome.png';
+
+
+	let name = "Boon"
+	let suggestions = $state([]);
+
+	const SearchEngine = {
+		Google: "https://www.google.com/search?q=",
+		DuckDuckGo: "https://duckduckgo.com/?q=",
+	}
+
+	const suggestionsAPI = 'https://api.datamuse.com/sug?s=';
+
+	let query = $state(``);
+
+	$effect(()=>{
+		if(query != ""){
+			fetch(suggestionsAPI + query + "&max=10").then((response) =>{
+				return response.json()
+			}).then((response) =>{
+				suggestions = response;
+			})
+		} else {
+			suggestions = []
+		}
+	})
+	function Search(e){
+		if (e.keyCode == 13){
+			window.location.href = `${SearchEngine.Google}${query}`
+		}
+	}
 </script>
 
 <svelte:head>
@@ -9,51 +36,40 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcomeFallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<div class="bg-black w-screen h-screen flex flex-col justify-center items-center text-white">
+	<div class="w-full grow-1">
+		<span>Test text</span>
+	</div>
+	<div class="w-full grow-2 flex flex-row">
+		<div class="h-full grow-1">
+			<span>Test text</span>
+		</div>
+		<div class="relative justify-center items-center h-full grow-2 bg-white/1 rounded-2xl flex flex-col">
+			<div class="grow-4 flex flex-col items-end justify-end">
+				<span class="font-bold mb-4 text-3xl">Hi, {name}</span>
+			</div>
+			<div class="grow-1">
+				<input type="text" bind:value={query}
+				class="text-center align-text-bottom text-2xl bg-black/85 border-b-2 border-white h-12 pl-2 w-64 rounded-b-sm outline-none duration-100 md:w-full font-bold"
+				onkeydown={Search}>
+			</div>
+			<div class="h-[200px]">
+				<div id="Search suggestion"
+					class="grow max-h-[200px] flex flex-col overflow-scroll">
+					{#if suggestions.length > 0}
+						{#each suggestions as suggestion}
+							<a href={`${SearchEngine.Google}${suggestion.word}`}>{suggestion.word}</a>
+						{/each}
+						{/if}
+				</div>
+			</div>
+		</div>
+		
+		<div class="h-full grow-1">
+			<span>Test text</span>
+		</div>
+	</div>
+	<div class="w-full grow-1">
+		<span>{query}</span>
+	</div>
+</div>
